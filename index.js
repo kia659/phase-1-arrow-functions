c1ba33ef8d0f8b7fd2fbe254596438b722b30c00
Kia Neitzell <kia.neitzell@gmail.com>
2024-02-02 06:36:50 -0800

    compleated

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (a, b) => a / b;
+let a = 2000;
+let b = 100;
+
+let resultDivide = divide(a, b);
+console.log(resultDivide);
+
+const square = (x) => x ** 2;
+let xSquare = 2;
+let resultSquare = square(xSquare);
+console.log(resultSquare);
+
+const add = (x, y) => x + y;
+let xAdd = 3;
+let yAdd = 4;
+let resultAdd = add(xAdd, yAdd);
+console.log(resultAdd);
